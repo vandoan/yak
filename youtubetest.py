f80e6b6d0d98d895dc5ced677d1d6dbfd4f49352
Van Doan <vdoan09@gmail.com>
2014-08-21 22:19:26 -0400

    updated py so that it is able to scrap titles and characters

--- youtubetest.py
+++ youtubetest.py
@@ -1,7 +1,25 @@
 import urllib 
+import re
 
-htmlfile = urllib.urlopen("http://google.com")
+# first v.: htmlfile = urllib.urlopen("http://google.com")
 
-htmltext= htmlfile.read()
+urls = ["http://google.com", "http://nytimes.com", "http://cnn.com"]
 
-print htmltext
+i=0
+regex = '<title>(.+?)</title>' 
+pattern = re.compile(regex)
+
+while i< len(urls) : 
+		htmlfile = urllib.urlopen(urls[i])
+		htmltext = htmlfile.read()
+		titles = re.findall(pattern,htmltext)
+
+#		print htmltext[0:100]
+		print titles
+		i+=1
+
+
+
+# first v.: htmltext= htmlfile.read()
+
+#print htmltext
