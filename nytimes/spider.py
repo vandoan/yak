f65b6dde3351178ffaac61bd31323c00ce1bea77
Van Doan <vdoan09@gmail.com>
2014-08-23 00:30:00 -0400

    updated spider

--- nytimes/spider.py
+++ nytimes/spider.py
@@ -1,20 +1,29 @@
+#page spider
 import urlparse 
 import urllib 
 from bs4 import BeautifulSoup
 
-url = "http://nytimes.com" 
+url = "http://www.nytimes.com" 
 
 urls =[url] # stack of urls to scrape 
 visited = [url] #historic record of urls
 
 while len(urls) >0: 
 	try: 
-		htmltext = urllib.urlopen(url[0]).read()
+		htmltext = urllib.urlopen(urls[0]).read()
 	except: 
 		print urls[0]
 	soup = BeautifulSoup(htmltext)
 
 	urls.pop(0)
+	print len(urls)
 
-	print  soup.findAll('a',hfref=True)
+	#print  soup.findAll('a',href=True)
+	for tag in soup.findAll('a',href=True): 
+		tag['href'] = urlparse.urljoin(url,tag['href'])
+	 	if url in tag['href'] and tag['href'] not in visited:
+	 		urls.append(tag['href'])
+	 		visited.append(tag['href'])
+
+print visited
 	  
